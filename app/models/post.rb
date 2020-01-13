f96cb8b90162516eadd30dbd936b4c0f096df979
Chris Kerbo <chris.kerbo@outlook.com>
2020-01-13 20:43:55 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: { minimum: 100 }
-  validates :category, presence: true if: "Fiction" || "Non-fiction"
+  validates :category, presence: true if: self.category == "Fiction" || "Non-fiction"
 end
